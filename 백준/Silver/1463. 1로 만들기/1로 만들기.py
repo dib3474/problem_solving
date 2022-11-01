ecc428617143df5c970b377d8898408d3016cdfc
dib3474 <95611065+dib3474@users.noreply.github.com>
2022-11-01 23:43:16 +0900

    [Silver III] Title: 1로 만들기, Time: 584 ms, Memory: 39088 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+X = int(input())
+
+d = [0 for _ in range(X+1)]
+
+for i in range(2, X+1):
+    d[i] = d[i-1] + 1
+    if i%2 == 0 and d[i] > d[i//2] + 1:
+        d[i] = d[i//2] + 1
+    if i%3 == 0 and d[i] > d[i//3] + 1:
+        d[i] = d[i//3] + 1
+print(d[X])
